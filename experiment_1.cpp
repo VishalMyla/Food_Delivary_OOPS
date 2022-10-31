3077b196ebffcf7ac88e2a027441f772aabcf30a
vishalmyla <vishalmyla@gmail.com>
2022-10-31 16:47:00 +0530

    Modified Cart and the Restarurent for the soomth user envirornament

--- experiment_1.cpp
+++ experiment_1.cpp
@@ -31,7 +31,10 @@ void Dish ::get_Details()
   cout << "The Price of the dish is : " << price << endl;
   cout << description << endl;
 }
-
+string Dish ::dish_Name()
+{
+  return Dish_name;
+}
 void Restaurant ::display_Details()
 {
   cout << "The name of the Restaurant is : " << Restaurant_name << endl;
@@ -40,9 +43,13 @@ void Restaurant ::display_Details()
   cout << "The name of the Restaurant is : " << Restaurant_name << endl;
   cout << "The Dishes available in the Restaurant" << endl
        << endl;
+  cout << endl;
   for (int i = 0; i < Array__of_Dishes.size(); i++)
   {
     Array__of_Dishes[i].get_Details();
+    cout << endl;
+    cout << "Enter " << i << " in the cart if you want to add thid Dish" << endl;
+    cout << endl;
     cout << endl;
   }
 }
@@ -55,50 +62,43 @@ string Restaurant ::get_Address()
   return Address;
 }
 
-int Cart::Restaurant_price()
+void Cart ::add_dish(int n)
 {
+  for (int i = 0; i < Array__of_Dishes.size(); i++)
+  {
+    if (n == i)
+    {
+      cout << "You have Picked " << Array__of_Dishes[i].dish_Name() << "in the cart : $" << Array__of_Dishes[i].get_price() << endl;
+      Price += Array__of_Dishes[i].get_price();
+    }
+  }
+  GST = add_tax(Price);
+  bill = Price + GST;
 }
-int Cart::Total_Amount()
+void Cart ::dispaly_BILL()
 {
+  cout << "The Price of the Dishes Ordered is : $" << Price << endl;
+  cout << "The Governament Tax : $" << GST << endl;
+  cout << "The Total Amount to be Paid is : $" << bill << endl;
 }
-
-void Cart::fill_cart()
+int Cart ::Payment()
 {
+  return bill;
 }
-void Cart::get_details()
-{
-}
-
-// class Cart
+// Tax is remainng
+// class Cart : public Restaurant
 // {
 // public:
-//   int Restaurant_price();
-//   int Total_Amount();
-//   void fill_cart();
-//   void get_details();
+//   void add_dish(int);
+//   void dispaly_BILL();
+//   int add_tax(int);
 
 // private:
-//   int Total_Price;
-//   vector<int> Picked;
-//   int Delivary_charges;
-//   int Govt_Tax;
+//   int Price;
+//   float GST;
+//   float bill;
 // };
 
-// class Restaurant
-// {
-// public:
-//   // Restaurant(string, int, string, int, Dish[10]);
-//   void display_Details();
-//   void add_Dish();
-//   string get_Address();
-
-// private:
-//   string Restaurant_name;
-//   int Restaurant_id;
-//   string Address;
-//   int No_Dishes_in_Restaurant;
-//   vector<Dish> Array__of_Dishes[10];
-// };
 int main()
 {
   cout << endl
